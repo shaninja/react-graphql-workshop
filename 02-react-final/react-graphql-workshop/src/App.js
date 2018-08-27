cc75d712ca661ab2a0cc953b440772526dba8208
Shani Raby <shani.raby@shani.raby-mbp>
2018-08-27 14:14:06 +0300

    Before changing structure of files

--- 02-react-final/react-graphql-workshop/src/App.js
+++ 02-react-final/react-graphql-workshop/src/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Photo from './Components/Photo/index.js';
+import {MOCK_DATA} from './Components/Constants/family-data.js';
 
 class FamilyAlbum extends Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+  getPersonalPhotoUrl() {
+    const data = JSON.parse(MOCK_DATA);
+    return data.data.personal_photo.url;
+  }
+
+  getGender() {
+    const data = JSON.parse(MOCK_DATA);
+    return data.data.gender;
+  }
+
   render() {
+    const a = MOCK_DATA;
     return (
       <div className="my_family_album">
         <header className="family_album_header">
@@ -11,9 +29,9 @@ class FamilyAlbum extends Component {
         </header>
         <section className="me_section">
           <div> This is me! </div>
-          <Photo 
-              url="https://www.myheritageimages.com/Z/storage/system/files/18/17/62/181762_154085dbd6add741bfq3of.jpg"
-              gender="M"
+          <Photo
+              // url={this.getPersonalPhotoUrl()}
+              // gender={this.getGender()}
             />
         </section>
         <section className="family_section">
--- 02-react-final/react-graphql-workshop/src/Components/Constants/family-data.js
+++ 02-react-final/react-graphql-workshop/src/Components/Constants/family-data.js
@@ -1,13 +1,180 @@
-export const MOCK_DATA = ```{
+export const MOCK_DATA = `{
   "data": {
-    "user": {
-      "name": "Developer MyHeritage",
+    "individual": {
+      "name": "Joseph Patrick Kennedy II",
       "gender": "M",
       "personal_photo": {
-        "url": "https://www.myheritageimages.com/Z/storage/system/files/18/17/62/181762_154085dbd6add741bfq3of.jpg"
+        "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/71/000271_7160282k66t34bp2befnda.jpg"
       },
-      "birth_date": null,
-      "country": "Great Britain"
+      "birth_date": {
+        "text": "Sep 4 1952"
+      },
+      "immediate_family": {
+        "data": [
+          {
+            "individual": {
+              "name": "Sheila Kennedy (born Rauch)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/75/000275_584246h926em2be4fd6bgs.jpg"
+              },
+              "birth_date": null
+            }
+          },
+          {
+            "individual": {
+              "name": "Anne Kennedy (born Kelly)",
+              "gender": "F",
+              "personal_photo": null,
+              "birth_date": null
+            }
+          },
+          {
+            "individual": {
+              "name": "Robert Francis \"Bobby\" Kennedy",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/55/000255_6890634d2f4bb652bdp2b6.jpg"
+              },
+              "birth_date": {
+                "text": "Nov 20 1925"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Ethel Kennedy (born Skakel)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/62/000262_1550832r6fd6e6bd32s4bv.jpg"
+              },
+              "birth_date": {
+                "text": "Apr 11 1928"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Kathleen Hartington Townsend (born Kennedy)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/68/000268_01199946t2a6ds2b04bhfe.jpg"
+              },
+              "birth_date": {
+                "text": "July 4 1951"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Robert Francis Kennedy, Jr.",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/78/000278_5902664bdee626lcbrnf2f.jpg"
+              },
+              "birth_date": {
+                "text": "Jan 17 1954"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "David Anthony Kennedy",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/96/000296_68586571dbbntd642522bf.jpg"
+              },
+              "birth_date": {
+                "text": "June 15 1955"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Mary Courtney Hill (born Kennedy)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/98/000298_422301224627x92hdbb2yf.jpg"
+              },
+              "birth_date": {
+                "text": "Sep 9 1956"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Michael Lemoyne Kennedy",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/03/02/000302_938140820yd2f4d2767bby.jpg"
+              },
+              "birth_date": {
+                "text": "Feb 27 1958"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Marry Kerry Kennedy",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/03/07/000307_325000b2f42z072i4b9d6l.jpg"
+              },
+              "birth_date": {
+                "text": "Sep 8 1959"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Christopher George Kennedy",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/03/15/000315_783590db24f369d27y29ba.jpg"
+              },
+              "birth_date": {
+                "text": "July 4 1963"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Matthew Maxwell Taylor Kennedy",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/03/17/000317_697700mbu3b26f1qd2d746.jpg"
+              },
+              "birth_date": {
+                "text": "Jan 11 1965"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Douglas Harriman Kennedy",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/03/18/000318_2272027bb6246f4v33t2do.jpg"
+              },
+              "birth_date": {
+                "text": "Mar 24 1967"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Rory Elizabeth Katherine Bailey (born Kennedy)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/03/19/000319_896425s242m7x6b3dfsbr2.jpg"
+              },
+              "birth_date": {
+                "text": "Dec 12 1968"
+              }
+            }
+          }
+        ]
+      }
     }
   }
-}```
+}`
