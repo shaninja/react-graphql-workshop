8781d7079d925f120205a0e9afb25eae6b39f11c
Shani Raby <shani.raby@shani.raby-mbp>
2018-08-28 13:55:53 +0300

    02-react-final to 02-react-solved, and removed subdir react-graphql-workshop

---
+++ 04-fg-final/src/Applications/FamilyAlbum/Constants/family-data.js
@@ -0,0 +1,196 @@
+export const MOCK_DATA = `
+{
+  "data": {
+    "individual": {
+      "name": "John Fitzgerald Kennedy",
+      "gender": "M",
+      "personal_photo": {
+        "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/00/03/000003_289190f6cib8579bd4n722.jpg"
+      },
+      "birth_date": {
+        "text": "May 29 1917"
+      },
+      "close_family": {
+        "data": [
+          {
+            "individual": {
+              "name": "John Fitzgerald 'John-John' Kennedy, Jr.",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/01/33/000133_9234706bb660c22f5dbbr4.jpg"
+              },
+              "birth_date": {
+                "text": "Nov 25 1960"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Patrick Bouvier Kennedy",
+              "gender": "M",
+              "personal_photo": null,
+              "birth_date": {
+                "text": "Aug 7 1963"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Caroline Bouvier Schlossberg (born Kennedy)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/01/21/000121_754789bn6bdj224264bffa.jpg"
+              },
+              "birth_date": {
+                "text": "Nov 27 1957"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Arabella Kennedy",
+              "gender": "F",
+              "personal_photo": null,
+              "birth_date": {
+                "text": "Aug 23 1956"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Joseph Patrick Kennedy",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/01/36/000136_6927846bbdqd261wfdb624.jpg"
+              },
+              "birth_date": {
+                "text": "Sep 6 1888"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Rose Kennedy (born Fitzgerald)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/01/44/000144_781992bz9d2ef46t6jb2b3.jpg"
+              },
+              "birth_date": {
+                "text": "July 22 1890"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Joseph Patrick Kennedy, Jr.",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/23/000223_516637c6pb2m6bdf214m8s.jpg"
+              },
+              "birth_date": {
+                "text": "July 28 1915"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Robert Francis 'Bobby' Kennedy",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/55/000255_6890634d2f4bb652bdp2b6.jpg"
+              },
+              "birth_date": {
+                "text": "Nov 20 1925"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Edward Moore (Ted) Kennedy",
+              "gender": "M",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/03/32/000332_190194rfb164424xbd7w25.jpg"
+              },
+              "birth_date": {
+                "text": "Feb 22 1932"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Rosemary Kennedy",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/26/000226_3904755d4kcz2apb66f26b.jpg"
+              },
+              "birth_date": {
+                "text": "Sep 13 1918"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Kathleen (Kick) Agnes 'Kick' Cavendish (born Kennedy)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/28/000228_788376rmfd9b24k066c0b2.jpg"
+              },
+              "birth_date": {
+                "text": "Feb 20 1920"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Eunice Kennedy Shriver (born Kennedy)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/33/000233_522045h66q2p8qbfbyc24d.jpg"
+              },
+              "birth_date": {
+                "text": "July 10 1921"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Patricia Lawford (born Kennedy)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/02/46/000246_658066r6dba2wbnf264dtn.jpg"
+              },
+              "birth_date": {
+                "text": "May 6 1924"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Jean Ann Smith (born Kennedy)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/03/22/000322_268055b2dfb3d7iwve2z46.jpg"
+              },
+              "birth_date": {
+                "text": "Feb 20 1928"
+              }
+            }
+          },
+          {
+            "individual": {
+              "name": "Jacqueline Lee Kennedy Onassis (born Bouvier)",
+              "gender": "F",
+              "personal_photo": {
+                "url": "https://www.myheritageimages.com/D/storage/site144530322/files/00/01/01/000101_3215192dzfa6862lbubb34.jpg"
+              },
+              "birth_date": {
+                "text": "July 28 1929"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+`
---
+++ 04-fg-final/src/Applications/FamilyAlbum/index.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import './index.css';
+import Photo from './Components/Photo/index.js';
+import Card from './Components/Card/index.js';
+import {MOCK_DATA} from './Constants/family-data.js';
+
+
+/**
+ * FamilyAlbum
+ * Page to display a person and details about his/hers family members
+ */
+class FamilyAlbum extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoriteFamilyMember: {}};
+  }
+
+  setFavorite(familyMemeberDetails) {
+    const {name, birth_date, personal_photo, gender} = familyMemeberDetails;
+    this.setState({favoriteFamilyMember: {
+      name: name,
+      birthDate: birth_date,
+      photoUrl: (personal_photo ? personal_photo.url : undefined),
+      gender: gender,
+    }});
+  }
+
+  render() {
+    const individualData = JSON.parse(MOCK_DATA).data.individual;
+    const familyMembers = individualData.close_family.data;
+    const {favoriteFamilyMember} = this.state;
+    return (
+      <div className="my_family_album">
+        <header className="family_album_header">
+          <h1 className="family_album_title">{individualData.name}'s Family</h1>
+        </header>
+        <section className="me_section">
+          <div className="personal_photo">
+              <Photo
+                url={individualData.personal_photo.url}
+                gender={individualData.gender}
+              />
+              </div>
+            <div className="favorite_family_memberr">
+              <Photo 
+                url={favoriteFamilyMember.photoUrl}
+                gender={favoriteFamilyMember.gender}
+              />
+            </div>
+        </section>
+        <section className="family_section">
+          <div className="family_members">
+            {
+              familyMembers.map(member =>
+                {
+                  const {name, birth_date, personal_photo, gender} = member.individual;
+                  return <Card
+                    key={name}
+                    name={name}
+                    birthDate={birth_date}
+                    photoUrl={personal_photo ? personal_photo.url : undefined}
+                    gender={gender}
+                    onClick={() => this.setFavorite(member.individual)}
+                  />;
+                })
+            }
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default FamilyAlbum;
