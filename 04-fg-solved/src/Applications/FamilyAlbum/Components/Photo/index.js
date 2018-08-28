d28f0955451f3ac5059d5a733956abc69c9ec0c0
Shani Raby <shani.raby@shani.raby-mbp>
2018-08-28 13:56:45 +0300

    04-fg-final to 04-fg-solved, and removed subdir react-graphql-workshop

---
+++ 04-fg-solved/src/Applications/FamilyAlbum/Components/Photo/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './index.css';
+import {
+  UNKNOWN,
+  GENDERS,
+  UNKNOWN_PHOTO,
+} from './constants.js';
+
+const propTypes = {
+    url: PropTypes.string,
+    gender: PropTypes.oneOf(GENDERS),
+};
+const defaultProps = {
+    url: UNKNOWN_PHOTO,
+    gender: UNKNOWN,
+};
+
+/**
+ * Photo Component
+ * Round photo with border that indicats the person't gender
+ * 
+ * @param {string} [url] Url for the photo that will be displayed
+ * @param {string} [gender] M/F/U
+ */
+class Photo extends React.PureComponent {
+  render() {
+    const {url, gender} = this.props;
+    const imageClass = `photo ${gender}`;
+
+    return (
+      <span className="photo_container">
+        <img 
+            className={imageClass} 
+            src={url} 
+            alt="Personal Photo"    
+        />
+      </span>
+    );
+  }
+}
+
+Photo.propTypes = propTypes;
+Photo.defaultProps = defaultProps;
+
+export default Photo;
