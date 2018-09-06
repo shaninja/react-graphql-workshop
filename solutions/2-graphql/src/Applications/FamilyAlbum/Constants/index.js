2ec30a9f3ccfd8df8527bedd248f5b17438fd33d
Shani Raby <shani.raby@shani.raby-mbp>
2018-09-06 13:13:56 +0300

    added TODOs on how to create tokens.js

--- solutions/2-graphql/src/Applications/FamilyAlbum/Constants/index.js
+++ solutions/2-graphql/src/Applications/FamilyAlbum/Constants/index.js
@@ -1,4 +1,17 @@
-
+/**
+ * TODO
+ * 1. Create a file named tokens.js in this directory.
+ * 2. type:
+ *    export const FAMILY_GRAPH_QL_TOKEN = 'YOUR_FGQL_TOKEN';
+ * 3. Go to http://www.familygraphql.com/fiddle 
+ * 4. Open the console
+ * 5. Type 'oauthToken'
+ * 6. Copy the token and paste it instead of YOUR_FGQL_TOKEN.
+ * The token is valid for 12 hours.
+ * DO NOT keep tokens.js in source control.
+ * 
+ * IRL you would get the token from the server.
+ */
 import {FAMILY_GRAPH_QL_TOKEN} from './tokens.js';
 
 export const FGQL_URL = () => 
